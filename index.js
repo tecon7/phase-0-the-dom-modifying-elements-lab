b6e3227e2570d72a8d7ac996b10a5e700213c5aa
Teddy Economou <teddyecon@gmail.com>
2023-01-11 22:22:58 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const getRidOfMain = document.querySelector('#main')
+main.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", 'victory')
+newHeader.innerHTML = "Teddy is the champion"
